1211cf31fdb2540a6b24a7706f96e9716d007334
poroh-kun <meta.poroh.kun@gmail.com>
2021-05-19 18:47:33 +0300

    export to csv

---
+++ TvsfTest/Misc/TypeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    public static class TypeExtensions
+    {
+        public static bool IsCanBeNull(this Type type)
+        {
+            return !type.IsValueType || (Nullable.GetUnderlyingType(type) != null);
+        }
+    }
+}
